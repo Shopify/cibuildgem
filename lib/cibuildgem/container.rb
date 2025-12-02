c6eaaa8efbb79796eddedfba31f6c2867feff342
Edouard CHIN <chin.edouard@gmail.com>
2025-12-03 00:21:07 +0100

    WIP

--- lib/cibuildgem/container.rb
+++ lib/cibuildgem/container.rb
@@ -11,7 +11,7 @@ module Cibuildgem
       system("podman image build -t cibuildgem -f #{dockerfile}", exception: true)
 
       puts "The current directory is: #{Dir.pwd}"
-      system("podman container run --rm -it #{volumes_mount} cibuildgem bash -i -c 'bundle env'")
+      system("podman container run --rm -it #{volumes_mount} cibuildgem bash -i -c 'cibuildgem package'")
       # system("podman container run --rm -it #{volumes_mount} cibuildgem bash -i -c 'ls -la -R'")
     end
 
